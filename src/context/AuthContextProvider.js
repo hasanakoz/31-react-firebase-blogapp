e9385907f424f69b5d24888d4501be444ef6c461
hasan <hasanakoz1986@gmail.com>
2022-12-10 00:32:46 +0300

    firebase  codes done

--- src/context/AuthContextProvider.js
+++ src/context/AuthContextProvider.js
@@ -1,4 +1,11 @@
-import React, { useContext, createContext } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithRedirect,
+} from "firebase/auth";
+import React, { useContext, createContext, useState, useEffect } from "react";
+import { auth } from "../utils/firebaseConfig";
 
 const AuthContext = createContext();
 
@@ -6,8 +13,65 @@ export function useAuth() {
   return useContext(AuthContext);
 }
 
-const AuthContextProvider = () => {
-  return <div>AuthContextProvider</div>;
+const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  const signUp = createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+
+  const signIn = signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+
+  const signInWithGoogle = signInWithRedirect(auth, provider);
+
+  const signOut = signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  const value = {
+    currentUser,
+    signUp,
+    signIn,
+    signOut,
+    signInWithGoogle,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {" "}
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContextProvider;
